16355831d15e0dec4ca225e054a6c3e6287205cb
SantiRomano03 <romanosantiagonicolas@gmail.com>
2026-07-11 17:01:26 -0300

    Modify app.py, add entity patient and blueprint

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import JWTManager
 from models.db import db
 from models.user import User
 from models.nurse import Nurse
+from models.patient import Patient
 from models.news_and_prevention import NewsAndPrevention
 
 
@@ -19,8 +20,14 @@ db.init_app(app)
 with app.app_context():
     from models.user import User
     from models.nurse import Nurse
+    from models.patient import Patient
     from models.news_and_prevention import NewsAndPrevention
     db.create_all()
+
+# Registro blueprints 
+from routes.patient_routes import patients_bp
+app.register_blueprint(patients_bp)
+
 
 if __name__ == '__main__':
     print("Running Paciente360 application...")

b298cae68741bbc13827c8253b9b2aa83e724539
rubenledesma10 <rubenledesma89@gmail.com>
2026-08-15 11:28:40 -0300

    fix bugs

--- frontend/src/api/appointments.js
+++ frontend/src/api/appointments.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import client from './client'
-
-export function getAppointmentsByPatient(patientId) {
-  return client.get(`/appointments/patient/${patientId}`)
-=======
 import client from './client';
 
 export function getAppointments() {
@@ -16,5 +10,4 @@ export function getAppointmentsByPatient(patientId) {
 
 export function confirmAppointment(id) {
   return client.patch(`/appointments/${id}/confirm`);
->>>>>>> fa8bc9f489614fffffee19b8dccef2123f380400
 }

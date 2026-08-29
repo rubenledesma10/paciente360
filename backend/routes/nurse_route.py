52335a909d71bf57e3581d216b79d712001e8acb
rubenledesma10 <rubenledesma89@gmail.com>
2026-08-29 11:47:29 -0300

    add crud nurse

--- backend/routes/nurse_route.py
+++ backend/routes/nurse_route.py
@@ -4,6 +4,9 @@ from datetime import date
 from models.db import db
 from models.user import User
 from models.nurse import Nurse
+from enums import RoleEnum
+from utils.role_required import role_required
+from utils.email_service import send_welcome_email_admin
 from werkzeug.utils import secure_filename
 import os
 
@@ -14,10 +17,13 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 @nurses_bp.route('/', methods=['POST'])
+@role_required(RoleEnum.ADMINISTRATOR)
 def create_nurse():
     try:
         if not request.is_json:
             return jsonify({"msg": "Missing JSON in request"}), 400
+
+        data = request.get_json()
         
         if User.query.filter_by(email=request.json.get('email')).first():
             return jsonify({"msg": "Email already exists"}), 400
@@ -54,8 +60,8 @@ def create_nurse():
             is_reference=request.json.get('is_reference', False),
             role='nurse'
         )
-        new_user.set_password(request.json.get('password'))
-        db.session.add(new_user)
+        password = data.get('password') or data.get('dni')
+        new_user.set_password(password)
         db.session.flush()
         if 'profile_photo' in request.files:
             profile_photo = request.files['profile_photo']
@@ -65,13 +71,19 @@ def create_nurse():
                 profile_photo.save(file_path)
                 new_user.profile_photo = file_path
 
+
         db.session.commit()
+        try:
+            send_welcome_email_admin(new_user.email, new_user.first_name)
+        except Exception as mail_error:
+            print(f"Error al enviar correo al enfermero: {mail_error}")
         return jsonify({"msg": "Nurse created successfully", "nurse_id": new_user.id}), 201
     except Exception as e:
         db.session.rollback()
         return jsonify({"msg": "Error creating nurse"}), 500
     
 @nurses_bp.route('/', methods=['GET'])
+@role_required(RoleEnum.ADMINISTRATOR)
 def get_nurses():
     try:
         nurses = Nurse.query.all()
@@ -81,6 +93,7 @@ def get_nurses():
     
 
 @nurses_bp.route('/<int:nurse_id>', methods=['PUT'])
+@role_required(RoleEnum.ADMINISTRATOR)
 def update_nurse(nurse_id):
     try:
         nurse = User.query.get(nurse_id)
@@ -161,13 +174,13 @@ def update_nurse(nurse_id):
         return jsonify({"msg": "Error updating nurse"}), 500
     
 @nurses_bp.route('/<int:nurse_id>', methods=['DELETE'])
+@role_required(RoleEnum.ADMINISTRATOR)
 def delete_nurse(nurse_id):
     try:
         nurse = User.query.get(nurse_id)
         if not nurse:
             return jsonify({"msg": "Nurse not found"}), 404
-        
-        db.session.delete(nurse)
+        nurse.is_activate=False
         db.session.commit()
         return jsonify({"msg": "Nurse deleted successfully"}), 200
     except Exception as e:
@@ -175,6 +188,7 @@ def delete_nurse(nurse_id):
         return jsonify({"msg": "Error deleting nurse"}), 500
     
 @nurses_bp.route('/<int:nurse_id>', methods=['GET'])
+@role_required(RoleEnum.ADMINISTRATOR)
 def get_nurse(nurse_id):
     try:
         nurse = User.query.get(nurse_id)
@@ -186,6 +200,7 @@ def get_nurse(nurse_id):
         return jsonify({"msg": "Error fetching nurse"}), 500
     
 @nurses_bp.route('/search', methods=['GET'])
+@role_required(RoleEnum.ADMINISTRATOR)
 def search_nurses():
     try:
         query = request.args.get('query', '')
@@ -202,7 +217,8 @@ def search_nurses():
     except Exception as e:
         return jsonify({"msg": "Error searching nurses"}), 500
     
-@nurses_bp.route('/<int:nurse_id>/toggle',methods=['PATCH'])
+@nurses_bp.route('/<int:nurse_id>/toggle',methods=['PATCH']) #activar
+@role_required(RoleEnum.ADMINISTRATOR)
 def toggle_nurse_status(nurse_id):
     try:
         nurse = User.query.get(nurse_id)
